6e9b9f9135c61808cf3ae0b15c63eab83aed45e3
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2025-03-09 11:42:04 +0200

    feat: create logic for computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+function generateRandomNumber() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+function getComputerChoice() {
+    let computerValue = generateRandomNumber();
+    console.log(`Generated computer value ${computerValue}`);
+    if (computerValue >=1 && computerValue <=3) {
+        return "rock";
+    } else if (computerValue >=4 && computerValue <=6) {
+        return "paper";
+    } else if (computerValue >=7 && computerValue <=10) {
+        return "scissor";
+    }
+}
